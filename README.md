d47f18800bf632efe15ea4381c3b4a712717837c
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2018-11-04 11:53:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Expander
+Basic URL expander written as a Flask app.
+
+To use:
+1. Set a `PLATFORM_URL` environment variable that contains the base URL running [Shortener](https://github.com/pennlabs/shortener) (Do not include a trailing slash in the URL).
+2. Run `python main.py` to run the app.
---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, redirect
+app = Flask(__name__)
+
+
+@app.route('/<slug>')
+def get_url(slug):
+    platform_url = os.environ.get("PLATFORM_URL", "")
+    return redirect("{}/{}/".format(platform_url, slug), code=302)
+
+
+if __name__ == '__main__':
+    app.run()
